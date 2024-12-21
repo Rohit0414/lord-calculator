3c2f54e71ae18c289666837cb0eb09d7ded0c0c5
Rohit Dhiman <dhimanrohit070@gmail.com>
2024-12-21 14:12:16 +0530

    svg for homepage done

--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,111 @@ import { useTheme } from "../context/ThemeContext";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const { theme } = useTheme();
   const { t } = useTranslation();
+
+  const image = {
+    src: "/book-svgrepo-com.svg",
+    width: 70,
+    height: 70,
+  };
+  const image1 = {
+    src: "/bulb.svg",
+    width: 70,
+    height: 70,
+  };
+  const image2 = {
+    src: "/calculator.svg",
+    width: 70,
+    height: 70,
+  };
+  const image3 = {
+    src: "/divide.svg",
+    width: 70,
+    height: 70,
+  };
+  const image4 ={
+    src:"/teacher.svg",
+    width: 70,
+    height: 70,
+  }
+
   const calculatorCards = [
     {
       title: t("tit"),
       description: t('des'),
-      emoji: "🧑‍🏫",
       href: "/educational-calculator",
       backgroundColor: theme === "dark" ? "bg-[#4285f4]" : "bg-[#4285f4]",
+      Image: (
+        <Image
+          src={image4.src}
+          height={image4.height}
+          width={image4.width}
+          alt="image description"
+        />
+      ),
     },
     {
       title: t('calculatorCards.0.title'),
       description: t('calculatorCards.0.description'),
-      emoji: "➗",
       href: "/educational-calculator/cgpa-to-percentage-calculator?filter=CGPA+to+percentage",
       backgroundColor: theme === "dark" ? "bg-[#20a971]" : "bg-[#20a971]",
+      Image: (
+        <Image
+          src={image3.src}
+          height={image3.height}
+          width={image3.width}
+          alt="image description"
+        />
+      ),
     },
     {
       title: t('calculatorCards.1.title'),
       description: t('calculatorCards.1.description'),
-      emoji: "🔢",
       href: "/educational-calculator/cgpa-to-percentage-calculator/percentage-to-cgpa-calculator?filter=Percentage+to+CGPA",
       backgroundColor: theme === "dark" ? "bg-[#4285f4]" : "bg-[#4285f4]",
+      Image: (
+        <Image
+          src={image2.src}
+          height={image2.height}
+          width={image2.width}
+          alt="image description"
+        />
+      ),
     },
     {
       title: t('calculatorCards.2.title'),
       description: t('calculatorCards.2.description'),
-      emoji: "💡",
       href: "/educational-calculator/cgpa-to-percentage-calculator/gpa-to-cgpa-calculator?filter=GPA+to+CGPA",
       backgroundColor: theme === "dark" ? "bg-[#cf8408]" : "bg-[#cf8408]",
+      Image: (
+        <Image
+          src={image1.src}
+          height={image1.height}
+          width={image1.width}
+          alt="image description"
+        />
+      ),
     },
     {
       title: t('calculatorCards.3.title'),
       description: t('calculatorCards.3.description'),
-      emoji: "📚",
       href: "/educational-calculator/cgpa-to-percentage-calculator/cgpa-to-gpa-calculator?filter=CGPA+to+GPA",
       backgroundColor: theme === "dark" ? "bg-[#5865f2]" : "bg-[#5865f2]",
+      Image: (
+        <Image
+          src={image.src}
+          height={image.height}
+          width={image.width}
+          alt="image description"
+        />
+      ),
     },
   ];
+
 
   const [isMounted, setIsMounted] = useState(false);
 
@@ -116,7 +180,7 @@ export default function Home() {
               className={`flex flex-col gap-3 relative border dark:bg-gray-800 text-white bg-white hover:scale-105 rounded-md shadow-lg transition overflow-hidden duration-300 ease-in-out p-4 cursor-pointer ${card.backgroundColor} group`}
             >
               <Link href={card.href}>
-                <div className=" text-left mb-2 text-4xl">{card.emoji}</div>
+                <div className=" text-left mb-2 text-4xl">{card.Image}</div>
 
                 <div className="align-items-center text-left h-32 overflow-y-auto">
                   <h2 className="text-xxl font-bold mb-2 group-hover:text-white text-black dark:text-gray-100 drop-shadow-sm">

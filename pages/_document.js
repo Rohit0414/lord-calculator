730e9b7315b911f10056e8e3ac5ae70aceec9ca9
Rohit Dhiman <dhimanrohit070@gmail.com>
2025-01-18 15:16:00 +0530

    Initialize the GSC code

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="0WCJSxDdqg3mGH8r1s0akINRCAXjC-C3toX39MmnS-Q"
+        />
+
+        {/* Google Analytics using next/script */}
+        <Script 
+          strategy="afterInteractive" 
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y6991S01P3" 
+        />
+        <Script 
+          id="google-analytics" 
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Y6991S01P3');
+          `}
+        </Script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />

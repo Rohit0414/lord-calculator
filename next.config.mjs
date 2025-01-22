1ac547435e5e59118b20d88673e460b0d8dc633e
Rohit Dhiman <dhimanrohit070@gmail.com>
2025-01-23 02:58:48 +0530

    sgpa to cgpa

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,30 @@ const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'es', 'fr', 'hi', 'it','pt','bn','ru','ko','ta','ml','ar','de','ja'],
-    defaultLocale: 'en', 
-    localeDetection: false, 
+    defaultLocale: 'en',
+    localeDetection: false,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*.webmanifest',
+        destination: '/favicon/site.webmanifest',
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*.webmanifest',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;

f5b6c6d996c32131b8f88cb62d8af1c9235a9113
Rohit <dhimanrohit070@gmail.com>
2025-01-25 22:16:07 +0530

    clarity script

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,20 @@ export default function Document() {
             gtag('config', 'G-Y6991S01P3');
           `}
         </Script>
+
+        {/* Clarity Tracking Script */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pyi6mc6ih7");
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />

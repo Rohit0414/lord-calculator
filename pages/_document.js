b61d47cead49e40a19be5c6b21a0fa0ff50553c5
Rohit <dhimanrohit070@gmail.com>
2025-01-26 14:25:45 +0530

    clarity script

--- pages/_document.js
+++ pages/_document.js
@@ -1,41 +1,39 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Favicon links */}
         <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
 
-        <meta
-          name="google-site-verification"
-          content="0WCJSxDdqg3mGH8r1s0akINRCAXjC-C3toX39MmnS-Q"
+        {/* Google site verification */}
+        <meta name="google-site-verification" content="0WCJSxDdqg3mGH8r1s0akINRCAXjC-C3toX39MmnS-Q" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y6991S01P3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y6991S01P3');
+            `,
+          }}
         />
 
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-Y6991S01P3"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-        >
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-Y6991S01P3');
-          `}
-        </Script>
-
-        {/* Clarity Tracking Script */}
-        <Script
-          strategy="afterInteractive"
+        {/* Microsoft Clarity Tracking Script */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
@@ -46,10 +44,6 @@ export default function Document() {
             `,
           }}
         />
-      </Head>
-      <body className="antialiased">
-        <Main />
-        <NextScript />
       </body>
     </Html>
   );

e7cc8ed789a4f1f73f45aa09d7e7bc6bc47e238d
Rohit <dhimanrohit070@gmail.com>
2025-01-30 13:08:04 +0530

    scientific-calculator

---
+++ pages/scientific-calculator/index.jsx
@@ -0,0 +1,7 @@
+export default function ScientificCalculator() {
+    return (
+        <div className="flex items-center justify-center h-screen text-xl font-semibold">
+            Coming Soon...
+        </div>
+    );
+}
